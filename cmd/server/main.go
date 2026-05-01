34611598687a5e2f97788ef01d23a34e312d646c
Seva Sh <skimin1992@hotmail.com>
2026-05-01 19:35:49 +0400

    RabbitMQ instance support STOMP added

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril server...")
+
+	// create a new connection to RabbitMQ
+	url := "amqp://guest:guest@localhost:5672/"
+	conn, err := amqp.Dial(url)
+	if err != nil {
+		log.Println("Error:", err)
+		return
+	}
+	defer conn.Close()
+
+	fmt.Println("Connection successful")
+
+	// listen for a signal
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	// wait until signal received
+	<-signalChan
+
+	fmt.Println("Program is Shutting Down")
 }
